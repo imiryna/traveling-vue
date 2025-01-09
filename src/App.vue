0b7b1effe8a30381d7d43a77cfc98fc7c1228107
Iryna Burlaka <i.am.blinova@gmail.com>
2025-01-09 13:57:32 +0100

    tw styl of button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import IButton from './components/IButton.vue'
-const darc = 'bg'
 </script>
 
 <template>
--- src/components/IButton.vue
+++ src/components/IButton.vue
@@ -1,3 +1,5 @@
 <template>
-  <button class="bg-[#FFA279]">Click me</button>
+  <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider-0,05">
+    Почати роботу
+  </button>
 </template>
