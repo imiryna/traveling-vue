b86f1c01948a4ad6e70d56ad9105e730819aebd3
Iryna Burlaka <i.am.blinova@gmail.com>
2025-01-17 11:19:18 +0100

    sliling

--- src/components/IButton.vue
+++ src/components/IButton.vue
@@ -1,4 +1,6 @@
 <template>
-  <button class="blok bg-[#FFA279] rounded-xl">Почати роботу</button>
+  <button class="blok bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider-0,05">
+    Почати роботу
+  </button>
 </template>
 //#FFA279 //rounded-xl py-3 px-10 text-white font-bold -tracking-wider-0,05
